a0c5f82cd7883dc8aeb7d9ad8b420f7fc394e131
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-21 16:15:28 +0530

    Bitwise Operator

--- Operators/Bitwise.dart
+++ Operators/Bitwise.dart
@@ -1,4 +1,5 @@
 void main(){
+
   int x = 10;
   int y = 12;
   print(x & y);   //AND:8
@@ -6,4 +7,5 @@ void main(){
   print(x ^ y);   //XOR:6
   print(x<<3);    //Left-shift:80
   print(y>>1);    //Right-shift:6
+  
 }
