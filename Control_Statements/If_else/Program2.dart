55b6501e430ecfcd500115c662e514d6311285fd
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-22 17:46:04 +0530

    if else if program1

---
+++ Control_Statements/If_else/Program2.dart
@@ -0,0 +1,11 @@
+void main(){
+  int budget = 600;
+  if(budget < 999){
+    print("Eligible for TCS NQT-Cognitive Exam:)");
+  }
+  else if(budget >= 999){
+    print("Eligible for TCS NQT-IT Exam:)");
+  }else{
+    print("Eligible for TCS NQT free Exam:)");
+  }
+}
