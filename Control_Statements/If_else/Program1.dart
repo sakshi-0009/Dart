72c2132b49b11ae6ed33b5830b436f6b13d717b6
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-22 17:38:25 +0530

    if else program1

---
+++ Control_Statements/If_else/Program1.dart
@@ -0,0 +1,13 @@
+void main(){
+
+    int x = 5;
+    int y = 7;
+    if((++x <= ++y) || (x++ > ++y)){
+        print("Both are same");
+    }else{
+        print("Not same");
+    }
+    print(x);   //6
+    print(y);   //8
+    
+}
