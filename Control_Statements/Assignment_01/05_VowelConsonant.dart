70f47c3fbe16b2c25c19e72b09d286169ddd2af6
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-24 00:08:04 +0530

    Vowel or Consonant

---
+++ Control_Statements/Assignment_01/05_VowelConsonant.dart
@@ -0,0 +1,8 @@
+void main(){
+  var char = 'D';
+  if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U') {
+    print("$char is a vowel.");
+  } else {
+    print("$char is a consonant.");
+  }
+}
