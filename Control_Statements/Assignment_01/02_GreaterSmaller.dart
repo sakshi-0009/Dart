3de03724b48c30d9cc70786eccd7fc76b3e8d261
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-23 23:30:30 +0530

    Greater or smaller than 10

---
+++ Control_Statements/Assignment_01/02_GreaterSmaller.dart
@@ -0,0 +1,10 @@
+void main(){
+  int num = 16;
+  if(num < 10){
+    print("$num is less than 10");
+  }else if(num > 10){
+    print("$num is greater than 10");
+  }else{
+    print("$num is equal to 10");
+  }
+}
