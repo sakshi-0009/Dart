51e8b975a1630f2d3071ef51d47e062a39910286
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-24 00:30:58 +0530

    Ticket price

---
+++ Control_Statements/Assignment_01/09_TicketPrice.dart
@@ -0,0 +1,12 @@
+void main(){
+  int n = 3;
+  if(n == 1){
+    print("For upper stand ticket please pay 2000 rupees.");
+  }if(n == 2){
+    print("For middle stand ticket please pay 3000 rupees.");
+  }if(n == 3){
+    print("For lower stand ticket please pay 7000 rupees.");
+  }else{
+    print("All other tickets for 2500");
+  }
+}
