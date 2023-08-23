db79f8209454d97fcc4e229e3b4d31beb77d99af
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-24 00:10:59 +0530

    Spelling of numbers

---
+++ Control_Statements/Assignment_01/06_Spelling.dart
@@ -0,0 +1,18 @@
+void main(){
+  int n = 4;
+  if(n == 0){
+    print("Zero");
+  }else if(n == 1){
+    print("One");
+  }else if(n == 2){
+    print("Two");
+  }else if(n == 3){
+    print("Three");
+  }else if(n == 4){
+    print("Four");
+  }else if(n == 5){
+    print("Five");
+  }else{
+    print("Entered number is greater than 5.");
+  }
+}
