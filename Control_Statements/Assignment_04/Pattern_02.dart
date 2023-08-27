de2efd1c05a59afe2e3bea20d63e2ab78f764bc5
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-27 10:10:10 +0530

    Pattern-2

---
+++ Control_Statements/Assignment_04/Pattern_02.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+  int n = 9;
+  for(int i = 1; i<=n; i++){
+    if(i%2==1){
+      for(int j=1; j<=n; j++){
+        if(j<=(n-i)){
+          stdout.write(" ");
+        }else{
+          stdout.write("* ");
+        }
+      }
+    }
+    print('');
+  }
+}
