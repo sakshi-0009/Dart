69566aa13b67fcc5b7155068906ab4f59392c524
sakshi-0009 <sakshigome1809@gmail.com>
2023-08-29 00:11:04 +0530

    Pattern-2

--- Control_Statements/Assignment_04/Pattern_02.dart
+++ Control_Statements/Assignment_04/Pattern_02.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
-void main(){
+void main() {
   int n = 9;
-  for(int i = 1; i<=n; i++){
-    if(i%2==1){
-      for(int j=1; j<=n; j++){
-        if(j<=(n-i)){
+  for (int i = 1; i <= n; i++) {
+    if (i % 2 == 1) {
+      for (int j = 1; j <= n; j++) {
+        if (j <= (n - i)) {
           stdout.write(" ");
-        }else{
+        } else {
           stdout.write("* ");
         }
       }
