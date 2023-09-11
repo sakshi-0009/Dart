6ec719b37a30d73ee70273bc600c35a6425c5923
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-11 15:51:02 +0530

    Recursion 01

---
+++ Functions/Program08.dart
@@ -0,0 +1,16 @@
+// Recursion :-
+
+// Program : Write for loop to print number from 5 to 1 in recursion:
+
+void fun(int n){
+  if(n==0){
+    return;
+  }
+  print(n);
+  n--;
+  fun(n);
+}
+
+void main(){
+  fun(10);
+}
