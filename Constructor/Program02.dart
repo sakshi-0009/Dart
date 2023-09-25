9628a3837391de46fef88e8d09e17c2bdf910d12
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-25 22:51:11 +0530

    Hidden This Reference 01

---
+++ Constructor/Program02.dart
@@ -0,0 +1,16 @@
+class Demo{
+  int? x;
+  String? str;
+  Demo(int x, String str){
+    this.x = x;
+    this.str = str;  
+  }
+  void printData(){
+    print(x);
+    print(str);
+  }
+}
+void main(){
+  Demo obj = new Demo(10,"Kanha");
+  obj.printData();
+}
