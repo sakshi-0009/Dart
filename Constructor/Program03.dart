01b96ac6331532f82a308944af2f4cf4e6d83948
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-26 06:29:05 +0530

    Printing hashCode values

--- Constructor/Program03.dart
+++ Constructor/Program03.dart
@@ -1,17 +1,17 @@
 class Demo{
   int x = 10;
   Demo(){
-    print(this.hashCode);  //116010
+    print(this.hashCode);  //743316739 and 720582600
   }
 }
 void main(){
   Demo obj1 = new Demo();
-  print(obj1.hashCode);       //243129021
-  print(identityHashCode(obj1)); //243129021
+  print(obj1.hashCode);       //743316739
+  print(identityHashCode(obj1)); //743316739
 
   Demo obj2 = new Demo();
-  print(obj2.hashCode);       //818297100
-  print(identityHashCode(obj2)); //818297100
+  print(obj2.hashCode);       //720582600
+  print(identityHashCode(obj2)); //720582600
 
   int x = 10;
   int y = 10;
