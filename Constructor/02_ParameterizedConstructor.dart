0a955ad4777895e1c3b2c02c62e5ae4d1eff07d8
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-26 22:22:30 +0530

    Parameterized Constructor

---
+++ Constructor/02_ParameterizedConstructor.dart
@@ -0,0 +1,10 @@
+class Employee{
+  int? empId;
+  String? empName;
+  Employee(this.empId, this.empName){
+    print("Parameterized Costructor");
+  }
+}
+void main(){
+  // Employee obj = new Employee(10,"Sakshi");
+}
