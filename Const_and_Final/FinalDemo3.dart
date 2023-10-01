bca3df9cf815efd8614f39b0b1b7f44e00373346
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-01 13:00:33 +0530

    final keyword

--- Const_and_Final/FinalDemo3.dart
+++ Const_and_Final/FinalDemo3.dart
@@ -9,7 +9,7 @@ void main(){
   //If we make object as const it points to same objects and gives same hashCode values.
   //It leads to efficient memory management and increases the performance.
 
-  Demo obj1 = const Demo(10,"kanha");   //838360209
+  Demo obj1 = const Demo(10,"Kanha");   //838360209
   Demo obj2 = const Demo(10,"Kanha");   //789726769
   print(obj1.hashCode);
   print(obj2.hashCode);
