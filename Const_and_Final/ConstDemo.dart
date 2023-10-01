3837e49f621de3dcc4d79dc3583c83dd0837a874
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-01 12:42:16 +0530

    const keyword

---
+++ Const_and_Final/ConstDemo.dart
@@ -0,0 +1,5 @@
+void main(){
+//  const int? x; //Error: The const variable 'x' must be initialized.Try adding an initializer ('= expression') to the declaration.
+const int? x = 10;
+  print(x);
+}
