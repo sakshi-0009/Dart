ada875f1b23a6e91f518dd440dcacee867f0896e
sakshi-0009 <sakshigome1809@gmail.com>
2023-11-20 10:14:10 +0530

    Quiz-02

---
+++ Map/Map Quiz/Quiz_02.dart
@@ -0,0 +1,10 @@
+import "dart:collection";
+void main(){
+  var mp = HashMap();
+  mp[1] = "Shashi";
+  mp[2] = "Bagal";
+
+  var mp2 = {4:"test",5:"test2",6:{1:"mp"}};
+  mp.addAll(mp2);
+  print(mp);
+}
