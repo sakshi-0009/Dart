c3984a2e8d9238b107c653b7db30fb8680dfe2da
sakshi-0009 <sakshigome1809@gmail.com>
2023-11-20 10:10:21 +0530

    Quiz-01

---
+++ Map/Map Quiz/Quiz_01.dart
@@ -0,0 +1,10 @@
+class Player{
+  String name;
+  int jerNo;
+  Player(this.name,this.jerNo);
+}
+void main(){
+  List<Player> player = [Player("MSD",7),Player("Rohit",45)];
+  List<String> names = player.map((player)=>player.name).toList();
+  print(names);
+}
