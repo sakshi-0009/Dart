b71d6efb8256798b00ec6e149aa01de1fd1c2f8a
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-11-22 23:44:17 +0530

    Quiz-01

--- Map/Map Quiz/Quiz_01.dart
+++ Map/Map Quiz/Quiz_01.dart
@@ -1,9 +1,9 @@
-class Player{
+class Player {
   String name;
   int jerNo;
   Player(this.name,this.jerNo);
 }
-void main(){
+void main() {
   List<Player> player = [Player("MSD",7),Player("Rohit",45)];
   List<String> names = player.map((player)=>player.name).toList();
   print(names);
